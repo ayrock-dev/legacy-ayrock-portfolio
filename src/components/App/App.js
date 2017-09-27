257333857678a47e974c0403996def55d6176eef
ayrock-dev <ayrocklee@gmail.com>
2017-09-27 14:32:00 -0500

    adding homepage template and Semantic UI React

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
+import HomepageLayout from '../HomepageLayout/HomepageLayout'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Get started
-        </p>
-      </div>
+      <HomepageLayout />
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './components/App/App';
 import registerServiceWorker from './components/registerServiceWorker/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
+
 registerServiceWorker();
