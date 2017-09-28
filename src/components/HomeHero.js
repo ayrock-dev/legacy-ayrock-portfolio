a48749ac07a65968149789b6361b4205f8e59171
ayrock-dev <ayrocklee@gmail.com>
2017-09-27 21:04:02 -0500

    added basics of page sections

--- src/components/HomeHero.js
+++ src/components/HomeHero.js
@@ -2,35 +2,49 @@ import React, { Component } from 'react'
 
 export default class HomeHero extends Component {
     render() {
-        return (
-            <div className="card">
-            <div className="card-image">
-              <figure className="image is-4by3">
-                <img src="http://placehold.it/1280x960"/>
-              </figure>
-            </div>
-            <div className="card-content">
-              <div className="media">
-                <div className="media-left">
-                  <figure className="image is-48x48">
-                    <img src="http://placehold.it/96x96"/>
-                  </figure>
-                </div>
-                <div className="media-content">
-                  <p className="title is-4">John Smith</p>
-                  <p className="subtitle is-6">@johnsmith</p>
+      return (
+        <section className="hero is-primary is-fullheight">
+          {/* Hero header: will stick at the top */}
+          <div id="top" className="hero-head">
+            <header className="nav">
+              <div className="container">
+                <span className="nav-toggle">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </span>
+                <div className="nav-right nav-menu">
+                  <a className="nav-item is-active" href="#top">
+                    Home
+                  </a>
+                  <a className="nav-item" href="#objective">
+                    Objective
+                  </a>
+                  <a className="nav-item">
+                    Portfolio
+                  </a>
+                  <span className="nav-item">
+                    <a className="button is-primary is-inverted">
+                      <span className="icon">
+                        <i className="fa fa-file-text"></i>
+                      </span>
+                      <span>Resume</span>
+                    </a>
+                  </span>
                 </div>
               </div>
-          
-              <div className="content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                <a href="#">#css</a> <a href="#">#responsive</a>
-                <br/>
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-              </div>
+            </header>
+          </div>
+
+          {/* Hero content: will be in the middle */}
+          <div className="hero-body">
+            <div className="container">
+              <h1 className="title">
+                <span>Eric Lee</span> is a JavaScript Guru
+              </h1>
             </div>
           </div>
-        )
+        </section>
+      )
     }
 }
--- src/components/HomepageLayout.js
+++ src/components/HomepageLayout.js
@@ -4,7 +4,25 @@ import HomeHero from './HomeHero'
 export default class HomepageLayout extends Component {
     render() {
       return (
-        <HomeHero />
+        <div>
+          <HomeHero />
+          <section id="objective" className="section">
+            <div className="container">
+              <h1 className="title">Objective</h1>
+              <h2 className="subtitle">
+                My objective is to collect all <strong>151</strong> Pokemon!
+              </h2>
+            </div>
+          </section>
+          <section id="portfolio" className="section">
+            <div className="container">
+              <h1 className="title">Portfolio</h1>
+              <h2 className="subtitle">
+                Here are some projects I have completed.
+              </h2>
+            </div>
+          </section>
+        </div>
       );
     }
   }
