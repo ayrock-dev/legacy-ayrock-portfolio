54f7bef172547dcb655628ccaff3b7033570aa71
Eric <ayrocklee@gmail.com>
2018-03-28 16:02:05 -0500

    relative PUBLIC_URL links

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -3,7 +3,15 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 export default class PortfolioCard extends Component {
-    getReadMoreLink = (id) => '/portfolio/' + id
+    getReadMoreLink = id => `${process.env.PUBLIC_URL}/portfolio/${id}`
+
+    getRelativeThumbUrl = thumbUrl => {
+        if ((thumbUrl || '').indexOf('http') >= 0) {
+            return thumbUrl
+        } else {
+            return `${process.env.PUBLIC_URL}/${thumbUrl}`
+        }
+    }
 
     render() {
         const { id, title, thumbUrl, description, hasContent, techIcons } = this.props
@@ -13,12 +21,10 @@ export default class PortfolioCard extends Component {
         let iconElements
 
         if (techIcons && techIcons.length) {
-            iconElements = techIcons.map( (icon, index) => {
+            iconElements = techIcons.map((icon, index) => {
                 const iconKey = 'icon-' + id + '-' + index
                 const iconClassName = 'is-large ' + icon.cssClass
-                return (
-                    <i className={iconClassName} key={iconKey} title={icon.tooltip}></i>
-                )
+                return <i className={iconClassName} key={iconKey} title={icon.tooltip} />
             })
         }
 
@@ -26,35 +32,25 @@ export default class PortfolioCard extends Component {
             <div className="card">
                 <div className="card-image">
                     <figure className="image">
-                        <img src={thumbUrl} alt="" />
+                        <img src={this.getRelativeThumbUrl(thumbUrl)} alt="" />
                     </figure>
                 </div>
                 <header className="card-header">
-                    <p className="card-header-title">
-                        {title}
-                    </p>
+                    <p className="card-header-title">{title}</p>
                 </header>
                 <div className="card-content">
                     <div className="content">
                         {description}
-                        {techIcons && 
-                            (   
-                                <p className="tech-icons has-text-right">
-                                    {iconElements}
-                                </p>
-                            )
-                        }
+                        {techIcons && <p className="tech-icons has-text-right">{iconElements}</p>}
                     </div>
                 </div>
-                { hasContent &&
+                {hasContent && (
                     <footer className="card-footer">
-                        <Link 
-                            to={readMoreLink} 
-                            className="card-footer-item">
+                        <Link to={readMoreLink} className="card-footer-item">
                             Read More
                         </Link>
                     </footer>
-                }
+                )}
             </div>
         )
     }
--- src/components/PrimaryMenu.js
+++ src/components/PrimaryMenu.js
@@ -1,34 +1,34 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import BulmaBurger from "./bulma/BulmaBurger";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import BulmaBurger from './bulma/BulmaBurger'
 
 export default class PrimaryMenu extends Component {
-  render() {
-    return (
-      <div className="nav">
-        <div className="container">
-          <BulmaBurger />
-          <div id="primaryNav" className="nav-right nav-menu">
-            <Link to="/" className="nav-item">
-              Home
-            </Link>
-            <a className="nav-item" href="#objective">
-              Objective
-            </a>
-            <a className="nav-item" href="#portfolio">
-              Portfolio
-            </a>
-            <span className="nav-item">
-              <a className="button is-primary is-inverted">
-                <span className="icon">
-                  <i className="fa fa-file-text" />
-                </span>
-                <span>Resume</span>
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="nav">
+                <div className="container">
+                    <BulmaBurger />
+                    <div id="primaryNav" className="nav-right nav-menu">
+                        <Link to={`${process.env.PUBLIC_URL}`} className="nav-item">
+                            Home
+                        </Link>
+                        <a className="nav-item" href="#objective">
+                            Objective
+                        </a>
+                        <a className="nav-item" href="#portfolio">
+                            Portfolio
+                        </a>
+                        <span className="nav-item">
+                            <a className="button is-primary is-inverted">
+                                <span className="icon">
+                                    <i className="fa fa-file-text" />
+                                </span>
+                                <span>Resume</span>
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
